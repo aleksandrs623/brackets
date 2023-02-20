7550ff2d79563d3efd010f6c9ba87ad6219e725b
Aleksandrs <alexandr.cursinov@gmail.com>
2023-02-20 16:19:56 +0200

    feat: add breckets function code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  for (let i = 0; i < str.length; i++) {
+    const bracket = str[i];
+    const config = bracketsConfig.find(c => c[0] === bracket || c[1] === bracket);
+    if (config) {
+      if (config[0] === config[1]) {
+        if (stack.length > 0 && stack[stack.length - 1] === bracket) {
+          stack.pop();
+        } else {
+          stack.push(bracket);
+        }
+      } else if (bracket === config[0]) {
+        stack.push(bracket);
+      } else if (stack.length === 0 || stack.pop() !== config[0]) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
 }
+  
